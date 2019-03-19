88d35db070e4aae9b981b195d320ca9d0e800c7e
lxb <mliuxb@163.com>
2019-03-19 17:28:31 +0800

    190319更新

---
+++ app/src/main/java/com/mliuxb/lxbweather/db/Province.java
@@ -0,0 +1,22 @@
+package com.mliuxb.lxbweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * Description:
+ * Copyright  : Copyright (c) 2018
+ * Company    : ZWZX
+ * Author     : Liuxb
+ * Date       : 2019/3/18 17:19
+ */
+public class Province extends LitePalSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    
+
+}
