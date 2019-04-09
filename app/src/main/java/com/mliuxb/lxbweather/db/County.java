28023ab15db1c150acf1062c4d71b0dea0fd654a
lxb <mliuxb@163.com>
2019-04-09 17:31:47 +0800

    init db

---
+++ app/src/main/java/com/mliuxb/lxbweather/db/County.java
@@ -0,0 +1,12 @@
+package com.mliuxb.lxbweather.db;
+
+/**
+ * Description:
+ * Copyright  : Copyright (c) 2018
+ * Company    : ZWZX
+ * Author     : Liuxb
+ * Date       : 2019/4/9 17:24
+ */
+public class County {
+    
+}
