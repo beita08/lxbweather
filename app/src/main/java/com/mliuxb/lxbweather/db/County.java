b9adbbd9857da44bf37ee94332a0fc4e96b2d6e9
lxb <mliuxb@163.com>
2019-04-12 17:33:02 +0800

    LitePal数据库建表

--- app/src/main/java/com/mliuxb/lxbweather/db/County.java
+++ app/src/main/java/com/mliuxb/lxbweather/db/County.java
@@ -1,5 +1,7 @@
 package com.mliuxb.lxbweather.db;
 
+import org.litepal.crud.LitePalSupport;
+
 /**
  * Description:
  * Copyright  : Copyright (c) 2018
@@ -7,6 +9,45 @@ package com.mliuxb.lxbweather.db;
  * Author     : Liuxb
  * Date       : 2019/4/9 17:24
  */
-public class County {
-    
+public class County extends LitePalSupport{
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
 }
